f9ca563c7cd9e6f918452ed338f0b3be35460a21
oinume <oinume@gmail.com>
2010-11-27 10:02:26 +0900

    make distiributable(setup.py)

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+import os
+
+def get_long_description():
+    file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README')
+    long_description = ''
+    try:
+        f = open(file)
+        long_description = ''.join(f.readlines())
+    except IOError:
+        print path + ': Failed to open file.'
+    finally:
+        f.close()
+    return long_description
+
+setup(
+    name = 'tomahawk',
+    version = '0.1.0',
+    url = 'http://github.com/oinume/tomahawk/',
+    license = 'GPL',
+    author = 'Kazuhiro Oinuma',
+    author_email = 'oinume@gmail.com',
+    description = 'A simple ssh wrapper for executing commands for many hosts.',
+    long_description = get_long_description(),
+    packages = [ 'tomahawk' ],
+    scripts = [ os.path.join('bin', p) for p in [ 'tomahawk_bootstrap.py', 'tomahawk.py', 'tomahawk_rsync.py' ] ],
+    zip_safe = False,
+    platforms = 'unix',
+    install_requires = [
+        'argparse>=1.1',
+        'multiprocessing>=2.6.1.1',
+        'pexpect>=2.4',
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GPL License',
+        'Operating System :: UNIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Systems Administration',
+    ],
+    #test_suite='__main__.run_tests'
+)
