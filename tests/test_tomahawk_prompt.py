12f05bc6be21da9ccb710c562de17ec7212f82bd
oinume <oinume@gmail.com>
2010-12-06 21:55:40 +0900

    added dummy tests. not implemented yet

---
+++ tests/test_tomahawk_prompt.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# mock_ssh 'sudo uptime'
+# req: GET /expect?command=sudo uptime
+# res: 200 Password:
+# req: GET /send?s=$xyz&command=sudo uptime
+# res: 200 uptime result
+
+pass
