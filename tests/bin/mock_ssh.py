cda2e1f47d440b44e25a6ece5837f23e09669a17
oinume <oinume@gmail.com>
2011-01-30 23:31:53 +0900

    add --prompt option

--- tests/bin/mock_ssh.py
+++ tests/bin/mock_ssh.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# A mock program of ssh command.
 
-import sys
+import argparse
+import os
 import re
 import shlex
 import subprocess
 
-regexp = re.compile("^/bin/sh -c '(.+)'$")
-for arg in sys.argv:
-#    print arg
-#    matches = regexp.search(arg)
-    if regexp.match(arg):
-        exit(subprocess.call(shlex.split(arg)))
+parser = argparse.ArgumentParser(
+    prog = os.path.basename(__file__),
+    description = 'A mock program of ssh command.',
+    conflict_handler = 'resolve'
+)
+parser.add_argument(
+    'command', metavar='command', nargs='+',
+    help='Command executed on remote hosts.',
+)
+parser.add_argument(
+    '-p', '--prompt', metavar='PROMPT_STRING',
+    help='Prompt with given string.',
+)
 
+options = parser.parse_args()
+if options.prompt:
+    input = raw_input(options.prompt)
+else:
+    regexp = re.compile("^/bin/sh -c '(.+)'$")
+    for arg in options.command:
+        if regexp.match(arg):
+            exit(subprocess.call(shlex.split(arg)))
