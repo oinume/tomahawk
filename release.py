fc57fea54649ffe8fb54c715df08e3f2c2a739ac
oinume <oinume@gmail.com>
2011-02-03 23:17:15 +0900

    add release script

---
+++ release.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# version input
+# Changes
+# tomahawk/constants.py VERSION = '0.2.2'
+# upload
+
+from tomahawk.constants import VERSION
+
+new_version = raw_input("Relase version [%s]: " % (VERSION))
+#tomahawk.
+
+
