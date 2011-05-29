c10fc6b09332073ce260d605c58a92cb85329d98
oinume <oinume@gmail.com>
2011-05-29 22:18:03 +0900

    Fix: pexpect hung up problem

--- tomahawk/expect.py
+++ tomahawk/expect.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-from cStringIO import StringIO
 from pexpect import spawn, EOF, TIMEOUT
-from sys import stderr
 from tomahawk.constants import DEFAULT_TIMEOUT, FatalError, TimeoutError
 from tomahawk.log import create_logger
 
@@ -24,73 +22,57 @@ class CommandWithExpect(object):
         
         Returns: command result status, output string
         """
-        output = StringIO()
-        #print("##### self.command = '%s', self.command_args = '%s'" % (self.command, self.command_args))
         child = spawn(
             self.command,
             self.command_args,
             timeout = self.timeout,
-            logfile = output, # TODO: logfile -> child.before() or child.after()
         )
-        #child.logfile_send = file('/tmp/expect_send', 'w')
-        #child.logfile = sys.stdout
         self.log.debug("command = %s, args = %s" % (self.command, str(self.command_args)))
 
-        login_expect = r"^(.+'s password:?\s*|Enter passphrase.+)"
-        sudo_expect0 = r'^[Pp]assword:?\s*$'
-        sudo_expect1 = '^([Pp]assword:?\s*|パスワード:\s*)' # TODO: japanese character expected as utf-8
-        sudo_expect2 = '^\[sudo\] password for.+$'
+        login_expect = '^(.+\'s password:?\s*|Enter passphrase.+)'
+        sudo_expect0 = '[Pp]assword:'
+        sudo_expect1 = '^パスワード:' # TODO: japanese character expected as utf-8
+
         try:
-            #print("##### before child.expect()")
-            index = child.expect([ login_expect, sudo_expect0, sudo_expect1, sudo_expect2 ])
+            index = child.expect([ login_expect, sudo_expect0, sudo_expect1, EOF ])
             self.log.debug("expect index = %d" % (index))
-            if index == 0: # login_expect
-                if self.login_password is None:
-                    # SSH authentication required but login_password is not input,
-                    # so notify --prompt-login-password option
-                    print >> stderr, '[warn] Use -l for ssh authentication.'
-                    child.sendline()
-                    child.expect(EOF)
-                    child.kill(0)
-                else:
-                    child.sendline(self.login_password)
-                    child.expect(EOF)
-            elif index in (1, 2, 3): # might be sudo_expect
-                #print "sudo expect. index = " + str(index)
-                # Because some OSes (like MacOS) prompt 'Password:' for SSH authentication,
-                # send login_password if sudo_password isn't provided
+
+            if index in (0, 1, 2):
                 password = self.sudo_password or self.login_password
                 if password is None:
+                    self.log.debug("Password is None")
                     #print >> stderr, "[error] Password is empty. Use -l or -s"
                     raise FatalError("Password is empty. Use -l or -s .")
+
                 child.sendline(password)
-                child.expect(EOF)
-            else:
-                raise FatalError("Should not reach here")
-
-        except EOF:
-            self.log.debug("expect.EOF")
-            pass
+            if index == 3:
+                self.log.debug("expect.EOF")
         except TIMEOUT:
-            self.log.debug("expect.TIMEOUT")
+            self.log.debug("timeout")
             raise TimeoutError("Execution is timed out after %d seconds" % (self.timeout))
+        except EOF:
+            self.log.debug("EOF")
+        finally:
+            #child.close()
+            pass
+        return self.get_status_and_output(child)
 
+    def get_status_and_output(self, child):
+        lines = child.readlines()
         child.close()
+        self.log.debug("child closed.")
+
         exit_status = child.exitstatus
-        #print "exit_status = %d" % exit_status
+        if exit_status is None:
+            exit_status = 1
+        self.log.debug("exit_status = %d" % exit_status)
 
-        output_tmp = output.getvalue()
         output_text = ''
-        password = self.login_password or self.sudo_password
-        if password is not None:
-            for line in output_tmp.splitlines():
-                if line.endswith(password):
-                    continue
-                output_text += line + '\n'
-        else:
-            output_text = output_tmp
-
-#        print "output = '%s'" % output_text
-        output.close()
+        for line in lines:
+            if line.strip() == '':
+                # Ignore empty line
+                continue
+            output_text += line
+        self.log.debug("output_text = '%s'" % output_text)
 
         return exit_status, output_text.strip()
