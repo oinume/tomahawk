074c1f927f27d99430c83e8bdea7f368698f6847
oinume <oinume@gmail.com>
2011-06-22 19:18:57 +0900

    Refactoring modules

---
+++ tomahawk/command.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+import argparse
+import getpass
+import os
+import signal
+import sys
+import time
+
+from tomahawk.common import BaseContext, BaseExecutor, BaseMain
+from tomahawk.constants import TimeoutError
+from tomahawk.expect import CommandWithExpect
+from tomahawk.utils import shutdown_by_signal
+
+class CommandContext(BaseContext):
+    """
+    Command context
+    """
+    def __init__(self, arguments, options):
+        self.arguments = arguments
+        self.options = options
+
+class CommandMain(BaseMain):
+    """
+    Main class for tomahawk
+    """
+
+    def __init__(self, file, bin_dir):
+        # setup self.log, self.arg_parser, self.options
+        super(CommandMain, self).initialize(file, self.create_argument_parser(file))
+        self.log.debug("options = " + str(self.options))
+        self.log.debug("arguments = " + str(self.options.command))
+
+    def do_run(self):
+        context = CommandContext(
+            self.options.command,
+            self.options.__dict__,
+        )
+        hosts = self.check_hosts()
+
+        # prompt when production environment
+        self.confirm_execution_on_production(
+            'Command "%s" will be executed to %d hosts. Are you sure? [yes/NO]: '
+            % (' '.join(context.arguments), len(hosts))
+        )
+
+        executor = CommandExecutor(context, self.log, hosts)
+        return executor.execute(context.arguments)
+
+    @classmethod
+    def create_argument_parser(cls, file):
+        parser = argparse.ArgumentParser(
+            prog = os.path.basename(file),
+            description = "A simple command executor for many hosts.",
+            conflict_handler = 'resolve'
+        )
+        parser.add_argument(
+            'command', metavar='command', nargs='+',
+            help='Command executed on remote hosts.',
+        )
+        parser.add_argument(
+            '--ssh', default='ssh', help='ssh program. (default: "ssh")'
+        )
+        parser.add_argument(
+            '-u', '--ssh-user', help='ssh user.'
+        )
+        parser.add_argument(
+            '-o', '--ssh-options', help='ssh options.'
+        )
+        parser.add_argument(
+            '-s', '--prompt-sudo-password', action='store_true',
+            help='Prompt a password for sudo.'
+        )
+        parser.add_argument(
+            '--no-sudo-password', action='store_true',
+            help='Never prompt a password for sudo.'
+        )
+        cls.add_common_arguments(parser)
+        return parser
+
+def _command(
+    command, command_args, login_password, sudo_password,
+    timeout, expect_delay, debug_enabled):
+    """
+    Execute a command.
+    """
+    # Trap SIGINT(Ctrl-C) to quit executing a command
+    signal.signal(signal.SIGINT, shutdown_by_signal)
+
+    return CommandWithExpect(
+        command, command_args, login_password,
+        sudo_password, timeout, expect_delay, debug_enabled
+    ).execute()
+
+class CommandExecutor(BaseExecutor):
+    """
+    Execute commands.
+
+    Args:
+    commands -- commands to execute.
+    
+    Returns: when rsync succeeds, return 0. When errors, return 1
+    """
+    def execute(self, commands):
+        if len(commands) == 0:
+            raise RuntimeError('1st argument "commands" length is 0')
+
+        options = self.context.options
+        #ssh = options.get('ssh') or 'ssh'
+        
+        ssh_user = options.get('ssh_user') or getpass.getuser()
+        ssh_options = ''
+        if options.get('ssh_options'):
+            ssh_options = options['ssh_options'] + ' '
+        ssh_options += '-l ' + ssh_user
+
+        async_results = []
+        for host in self.hosts:
+            for command in commands:
+                command_args = []
+                for option in ssh_options.split(' '):
+                    #  remove left and right whitespaces
+                    command_args.append(option.strip())
+
+                command_args.append(host)
+                c = command.replace('"', '\\"')
+                # execute a command with shell because we want to use pipe(|) and so on.
+                command_args.extend([ '/bin/sh', '-c', '"%s"' % (c) ])
+
+                # host, command, ssh_user, ssh_option, login_password, sudo_password
+                async_result = self.process_pool.apply_async(
+                    _command,
+                    ( 'ssh', command_args, self.login_password,
+                      self.sudo_password, options['timeout'],
+                      options['expect_delay'], options['debug'] ),
+                )
+                async_results.append({ 'host': host, 'command': command, 'async_result': async_result })
+
+                if options['delay'] != 0:
+                    time.sleep(options['delay'])
+
+        hosts_count = len(self.hosts)
+        finished = 0
+        error_hosts = {}
+        while finished < hosts_count:
+            for dict in async_results:
+                host = dict['host']
+                async_result = dict['async_result']
+                if not async_result.ready():
+                    continue
+
+                exit_status = 1
+                command_output = ''
+                timeout_detail = None
+                try:
+                    exit_status, command_output = async_result.get(timeout = options['timeout'] + 1)
+                except TimeoutError, error:
+                    timeout_detail = str(error)
+                async_results.remove(dict)
+                finished += 1
+
+                output_params = {
+                    'user': ssh_user,
+                    'host': host,
+                    'command': dict['command'],
+                    'output': command_output,
+                }
+                # output template
+                # TODO: specify from command line option
+                output = '%(user)s@%(host)s %% %(command)s\n%(output)s' % output_params
+                if exit_status == 0:
+                    print output, '\n'
+                elif timeout_detail is not None:
+                    output += '[error] Command timed out after %d seconds' % (options['timeout'])
+                    print output, '\n'
+                    error_hosts[host] = 2
+                    if self.raise_error:
+                        print >> sys.stderr, '[error] Command "%s" timed out on host "%s" after %d seconds' % (command, host, options['timeout'])
+                        return 1
+                else:
+                    output += '[error] Command failed ! (status = %d)' % exit_status
+                    print output, '\n'
+                    error_hosts[host] = 1
+                    if self.raise_error:
+                        #raise RuntimeError("[error] Command '%s' failed on host '%s'" % (command, host))
+                        print >> sys.stderr, '[error] Command "%s" failed on host "%s"' % (command, host)
+                        return 1
+
+        if len(error_hosts) != 0:
+            hosts = ''
+            for h in self.hosts:
+                if h in error_hosts:
+                    hosts += '  %s\n' % (h)
+            hosts = hosts.rstrip()
+            print >> sys.stderr, '[error] Command "%s" failed on following hosts\n%s' % (command, hosts)
+            return 1
+        
+        return 0
---
+++ tomahawk/common.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+import multiprocessing
+import os
+import sys
+
+from tomahawk.constants import (
+    VERSION,
+    DEFAULT_TIMEOUT,
+    DEFAULT_EXPECT_DELAY,
+    DEFAULT_EXPECT_ENCODING,
+)
+from tomahawk.log import create_logger
+from tomahawk.utils import (
+    read_login_password,
+    read_sudo_password,
+)
+class BaseContext(object):
+    pass
+
+class BaseMain(object):
+    def __init__(self, file, bin_dir):
+        pass
+    
+    def initialize(self, script_path, arg_parser):
+        self.script_path = script_path
+        self.arg_parser = arg_parser
+        self.options = self.arg_parser.parse_args()
+        self.log = create_logger(self.options.debug)
+
+    def run(self):
+        try:
+            if self.options.profile:
+                file = '%s.prof.%d' % (os.path.basename(self.script_path), os.getpid())
+                cProfile = __import__('cProfile')
+                pstats = __import__('pstats')
+                cProfile.runctx("self.do_run()", globals(), locals(), file)
+                p = pstats.Stats(file)
+                p.strip_dirs()
+                p.sort_stats('time', 'calls')
+                p.print_stats()
+                return 0 # TODO: return exit status
+            else:
+                return self.do_run()
+        except KeyboardInterrupt:
+            print
+            print "Keyboard interrupt. exiting..."
+
+    def do_run(self):
+        raise Exception("This is a template method implemented by sub-class")
+
+    def check_hosts(self):
+        options = self.options.__dict__
+        if options.get('hosts') is not None and options.get('hosts_files') is not None:
+            self.log.error("Cannot specify both options --hosts and --hosts-files.")
+            self.log.error(self.arg_parser.format_usage())
+            sys.exit(2)
+
+        # initialize target hosts with --hosts or --hosts-files
+        hosts = []
+        # TODO: \, escape handling
+        # regexp: [^\\],
+        if options.get('hosts'):
+            list = options['hosts'].split(',')
+            for host in list:
+                host.strip()
+                hosts.append(host)
+        elif options.get('hosts_files'):
+            list = options['hosts_files'].split(',')
+            for file in list:
+                try:
+                    for line in open(file):
+                        host = line.strip()
+                        if host == '' or host.startswith('#'):
+                            continue
+                        hosts.append(host)
+                except IOError, e:
+                    print >> sys.stderr, "Failed to open '%s'. (%s)" % (file, e)
+                    sys.exit(4)
+        else:
+            self.log.error("Specify --hosts or --hosts-files option.")
+            self.log.error(self.arg_parser.format_usage())
+            sys.exit(2)
+
+        return hosts
+
+    def confirm_execution_on_production(self, message):
+        if os.environ.get('TOMAHAWK_ENV') != 'production':
+            return
+
+        input = raw_input(message)
+        if input == 'yes':
+            print
+        else:
+            print "Command execution was cancelled."
+            sys.exit(0)
+
+    @classmethod
+    def add_common_arguments(cls, parser):
+        parser.add_argument(
+            '-h', '--hosts', metavar='HOSTS',
+            help='Host names for sending commands. (splited with ",")',
+        )
+        parser.add_argument(
+            '-f', '--hosts-files', metavar='HOST_FILE',
+            help='Hosts files which listed host names. (splited with ",")'
+        )
+        parser.add_argument(
+            '-c', '--continue-on-error', action='store_true', default=None,
+            help='Command exectuion continues whatever any errors.'
+        )
+        parser.add_argument(
+            '-p', '--parallel', metavar='NUM', type=int, default=1,
+            help='Process numbers for parallel command execution. (default: 1)'
+        )
+        parser.add_argument(
+            '-l', '--prompt-login-password', action='store_true',
+            help='Prompt a password for ssh authentication.'
+        )
+        parser.add_argument(
+            '-t', '--timeout', metavar='SECONDS', type=int, default=DEFAULT_TIMEOUT,
+            help='Specify expect timeout in seconds. (default: %d)' % (DEFAULT_TIMEOUT)
+        )
+        parser.add_argument(
+            '--expect-timeout', metavar='SECONDS', type=int,
+            help='Duplicated. Use --timeout'
+        )
+        parser.add_argument(
+            '--expect-encoding', metavar='ENCODING', default=DEFAULT_EXPECT_ENCODING,
+            help='Expect encoding for password prompt. (default: %s)' % (DEFAULT_EXPECT_ENCODING)
+        )
+        parser.add_argument(
+            '-d', '--delay', type=int, default=0,
+            help='Command delay time in seconds. (default: 0)'
+        )
+        parser.add_argument(
+            '--expect-delay', type=float, default=DEFAULT_EXPECT_DELAY,
+            help='Expect delay time in seconds. (default: 0.05)'
+        )
+        parser.add_argument(
+            '-D', '--debug', action='store_true', default=False,
+            help='Enable debug output.',
+        )
+        parser.add_argument(
+            '--profile', action='store_true', help='Enable profiling.'
+        )
+        parser.add_argument(
+            '--version', action='version', version='%(prog)s ' + VERSION
+        )
+        return parser
+
+class BaseExecutor(object):
+    """
+    A base class for CommandExecutor, RsyncExecutor
+    """
+    def __init__(self, context, log, hosts=[], **kwargs):
+        """
+        Constructor
+        
+        Args:
+        context -- context
+        log -- log
+        hosts -- target hosts
+        """
+        if context is None:
+            raise RuntimeError('Argument "context" required.')
+        if len(hosts) == 0:
+            raise RuntimeError('Argument "hosts" length must be > 0')
+
+        options = context.options
+        if options['expect_timeout'] is not None:
+            options['timeout'] = options['expect_timeout']
+            log.warn("Option --expect-timeout is duplicated. Use --timeout.")
+
+        newline = False
+        login_password = None
+        if 'login_password' in kwargs:
+            login_password = kwargs['login_password']
+        elif options.get('prompt_login_password'):
+            login_password = read_login_password()
+            newline = True
+        
+        sudo_password = None
+        if 'sudo_password' in kwargs:
+            sudo_password = kwargs['sudo_password']
+        elif options.get('prompt_sudo_password') \
+             or (not options.get('no_sudo_password') \
+                 and context.arguments is not None
+                 and context.arguments[0].startswith('sudo')):
+            sudo_password = read_sudo_password()
+            newline = True
+
+        if newline:
+            print
+
+        self.context = context
+        self.log = log
+        self.hosts = hosts
+        self.login_password = login_password
+        self.sudo_password = sudo_password
+        self.raise_error = False if options['continue_on_error'] else True
+        self.process_pool = multiprocessing.Pool(processes = options['parallel'])
+        
+    def destory_process_pool(self):
+        if self.process_pool is not None:
+            self.process_pool.close()
+
+    def __del__(self):
+        self.destory_process_pool()
