460efc6366defd744fa7461010ca65c5fe6e452b
oinume <oinume@gmail.com>
2012-03-13 21:07:59 +0900

    version++

--- tomahawk/__init__.py
+++ tomahawk/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Kazuhiro Oinuma'
 __author_email__ = 'oinume@gmail.com'
 __license__ = 'LGPL',
-__version__ = '0.5.0-rc1'
+__version__ = '0.5.0-rc2'
 __maintainer__ = 'Kazuhiro Oinuma'
 __status__ = 'Production/Stable'
