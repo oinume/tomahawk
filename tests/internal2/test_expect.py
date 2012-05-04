3dcffe08a2a65895aa74558a82b93f7c619ebda6
oinume <oinume@gmail.com>
2012-05-05 01:11:55 +0900

    Add test

--- tests/internal2/test_expect.py
+++ tests/internal2/test_expect.py
@@ -5,68 +5,34 @@ import pytest
 import utils
 utils.append_home_to_path(__file__)
 
-import tomahawk.expect
-import tomahawk.constants
+from tomahawk.expect import CommandWithExpect
+from tomahawk.constants import TimeoutError
 
 def test_00_execute():
+    """Normal"""
     expect_out = cStringIO.StringIO()
     target = create_object(expect_out)
     expect_out.write("hello world\n")
     expect_out.write("Connection to localhost closed\n")
 
     status, output = target.execute()
-    assert(status == 0)
-    assert(output == "hello world")
+    assert status == 0
+    assert output == "hello world"
 
 def test_01_execute_timeout():
+    """Timeout"""
     target = create_object(cStringIO.StringIO())
-    MockPexpect.expect = Mock(side_effect = pexpect.TIMEOUT('timeout'))
-    pytest.raises(tomahawk.constants.TimeoutError, target.execute)
+    utils.MockPexpect.expect = Mock(side_effect = pexpect.TIMEOUT('timeout'))
+    pytest.raises(TimeoutError, target.execute)
 
 def create_object(expect_out):
     command = 'ssh'
     command_args = [ '-t' ]
-    expect = MockPexpect(
+    expect = utils.MockPexpect(
         command, command_args, logfile = expect_out
     )
-    return tomahawk.expect.CommandWithExpect(
+    return CommandWithExpect(
         command, command_args, 'password1', 'password2', debug_enabled = True,
         expect = expect, expect_out = expect_out
     )
 
-class MockPexpect(object):
-    def __init__(
-        self, command, args = [], timeout = 30, maxread = 2000,
-        searchwindowsize = None, logfile = None, cwd = None, env = None):
-        self.command = command
-        self.args = args
-        self.timeout = timeout
-        self.maxread = maxread
-        self.searchwindowsize = searchwindowsize
-        self.logfile = logfile
-        self.cwd = cwd
-        self.env = env
-        self._exitstatus = 0
-
-    def expect(self, pattern, timeout = -1, searchwindowsize = -1):
-        #sys.stdout.write("password: ")
-        return 0
-
-    def sendline(self, s = ''):
-        if self.logfile:
-            self.logfile.write(s + '\n')
-        pass
-
-    def send(self, s):
-        pass
-
-    def close(self):
-        pass
-
-    def get_exitstatus(self):
-        return self._exitstatus
-
-    def set_exitstatus(self, exitstatus):
-        self._exitstatus = exitstatus
-
-    exitstatus = property(get_exitstatus, set_exitstatus)
--- tests/internal2/utils.py
+++ tests/internal2/utils.py
@@ -1,3 +1,4 @@
+import cStringIO
 import os
 import sys
 
@@ -14,3 +15,65 @@ def get_bin_dir(file):
 
 def append_home_to_path(file):
     sys.path.insert(0, get_home_dir(file))
+
+append_home_to_path(__file__)
+from tomahawk.expect import CommandWithExpect
+from tomahawk.constants import (
+    DEFAULT_TIMEOUT,
+    DEFAULT_EXPECT_DELAY
+)
+
+class MockPexpect(object):
+    def __init__(
+        self, command, args = [], timeout = 30, maxread = 2000,
+        searchwindowsize = None, logfile = None, cwd = None, env = None
+    ):
+        self.command = command
+        self.args = args
+        self.timeout = timeout
+        self.maxread = maxread
+        self.searchwindowsize = searchwindowsize
+        self.logfile = logfile
+        self.cwd = cwd
+        self.env = env
+        self._exitstatus = 0
+
+    def expect(self, pattern, timeout = -1, searchwindowsize = -1):
+        #sys.stdout.write("password: ")
+        return 0
+
+    def sendline(self, s = ''):
+        if self.logfile:
+            self.logfile.write(s + '\n')
+        pass
+
+    def send(self, s):
+        pass
+
+    def close(self):
+        pass
+
+    def get_exitstatus(self):
+        return self._exitstatus
+
+    def set_exitstatus(self, exitstatus):
+        self._exitstatus = exitstatus
+
+    exitstatus = property(get_exitstatus, set_exitstatus)
+
+class MockCommandWithExpect(CommandWithExpect):
+    def __init__(
+        self, command, command_args, login_password, sudo_password,
+        timeout = DEFAULT_TIMEOUT, expect_delay = DEFAULT_EXPECT_DELAY,
+        debug_enabled = False, expect = None,
+        expect_out = cStringIO.StringIO()
+    ):
+        if expect is None:
+            expect = MockPexpect(command, command_args, timeout)
+        super(MockCommandWithExpect, self).__init__(
+            command, command_args, login_password, sudo_password,
+            timeout, expect_delay, debug_enabled, expect, expect_out
+        )
+
+    def execute(self):
+        return 0, ''
