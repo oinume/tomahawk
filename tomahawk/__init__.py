dd643290c2b0b644a9031e7984182006b7caa59c
oinuma-kazuhiro <oinuma_kazuhiro@cyberagent.co.jp>
2013-04-20 19:50:18 +0900

    Update copyright

--- tomahawk/__init__.py
+++ tomahawk/__init__.py
@@ -2,7 +2,7 @@ from tomahawk.constants import *
 
 __author__ = 'Kazuhiro Oinuma'
 __author_email__ = 'oinume@gmail.com'
-__copyright__ = '2011-2012'
+__copyright__ = '2011-2013'
 __license__ = 'LGPL',
 __version__ = '0.6.0-rc1'
 __maintainer__ = 'Kazuhiro Oinuma'
