802ad1f581445de874962c714295e6882027f7a6
Kazuhiro Oinuma <oinume@gmail.com>
2013-04-21 02:24:04 +0900

    bump version to 0.6.0

--- tomahawk/__init__.py
+++ tomahawk/__init__.py
@@ -4,7 +4,7 @@ __author__ = 'Kazuhiro Oinuma'
 __author_email__ = 'oinume@gmail.com'
 __copyright__ = '2011-2013'
 __license__ = 'LGPL',
-__version__ = '0.6.0-rc1'
+__version__ = '0.6.0'
 __maintainer__ = 'Kazuhiro Oinuma'
 __status__ = 'Production/Stable'
 
