91b090adba651a7850b63a6f7b3f1b2eca7d5695
Kazuhiro Oinuma <oinume@gmail.com>
2014-03-02 16:20:44 +0900

    bump version

--- tomahawk/__init__.py
+++ tomahawk/__init__.py
@@ -2,9 +2,9 @@ from tomahawk.constants import *
 
 __author__ = 'Kazuhiro Oinuma'
 __author_email__ = 'oinume@gmail.com'
-__copyright__ = '2011-2013'
+__copyright__ = '2011-2014'
 __license__ = 'LGPL',
-__version__ = '0.7.0-rc1'
+__version__ = '0.7.0'
 __maintainer__ = 'Kazuhiro Oinuma'
 __status__ = 'Production/Stable'
 
