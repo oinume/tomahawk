ae6d6b165190893a2b197b3a684c7dd82861fc91
Kazuhiro Oinuma <oinume@gmail.com>
2016-01-23 23:32:57 +0900

    Add Python3.5 support

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: System :: Clustering",
         "Topic :: System :: Systems Administration",
